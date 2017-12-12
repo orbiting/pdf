a20d7a67063b8978892b3689b9933fe514347f73
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-12-12 15:10:51 +0100

    setup express with nodemon restart

--- src/index.js
+++ src/index.js
@@ -4,31 +4,48 @@ import React from 'react'
 import { Page, Text, View, Document, StyleSheet } from '@react-pdf/core'
 import ReactPDF from '@react-pdf/node'
 
-// Create styles
-const styles = StyleSheet.create({
-  page: {
-    flexDirection: 'row',
-    backgroundColor: '#E4E4E4'
-  },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1
-  }
+import express from 'express'
+
+const PORT = process.env.PORT || 3007
+
+const server = express()
+
+server.get('/:slug*', (req, res) => {
+  console.log(req.params)
+
+  // Create styles
+  const styles = StyleSheet.create({
+    page: {
+      flexDirection: 'row',
+      backgroundColor: '#E4E4E4'
+    },
+    section: {
+      margin: 10,
+      padding: 10,
+      flexGrow: 1
+    }
+  })
+
+  // Create Document Component
+  const MyDocument = () => (
+    <Document>
+      <Page size='A4' style={styles.page}>
+        <View style={styles.section}>
+          <Text>Section #1</Text>
+        </View>
+        <View style={styles.section}>
+          <Text>Section #2</Text>
+        </View>
+      </Page>
+    </Document>
+  )
+
+  ReactPDF.render(<MyDocument />, `${__dirname}/example.pdf`)
+
+  return res.send('Done')
 })
 
-// Create Document Component
-const MyDocument = () => (
-  <Document>
-    <Page size='A4' style={styles.page}>
-      <View style={styles.section}>
-        <Text>Section #1</Text>
-      </View>
-      <View style={styles.section}>
-        <Text>Section #2</Text>
-      </View>
-    </Page>
-  </Document>
-)
-
-ReactPDF.render(<MyDocument />, `${__dirname}/example.pdf`)
+server.listen(PORT, (err) => {
+  if (err) throw err
+  console.log(`> Ready on port ${PORT}`)
+})
