8fad0541a6b736b2cfac80e486944889468405bb
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-02-08 17:21:29 +0100

    rm local disk cache
    already cached by react-pdf

--- src/lib/fonts.js
+++ src/lib/fonts.js
@@ -13,46 +13,23 @@ import https from 'https'
 import fs from 'fs'
 import { basename, join } from 'path'
 
-const DEV = process.env.NODE_ENV !== 'production'
+const { NODE_ENV, FONTS = "{}" } = process.env
 
-if (DEV) {
+if (NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const FONTS = JSON.parse(process.env.FONTS);
+const fonts = JSON.parse(FONTS || "{}")
 
-const cacheFont = (url, bname, cb) => {
-  const dest = join(__dirname, `fonts-cache-${bname || basename(url)}`)
-  if (fs.existsSync(dest)) {
-    cb(dest)
-  }
-
-  const file = fs.createWriteStream(dest)
-
-  https.get(url, response => {
-    response.pipe(file)
-    file.on('finish', () => {
-      file.close(() => {
-        cb(dest)
-      })
+Object.keys(fonts).forEach(key => {
+  const font = fonts[key]
+  if (font.url) {
+    Font.register(font.url, {
+      family: font.family
     })
-  })
-}
+  }
+})
 
-export const registerFonts = () => {
-  Object.keys(FONTS).forEach(key => {
-    const font = FONTS[key]
-    if (font.url) {
-      cacheFont(font.url, font.basename, file => {
-        Font.register(file, {
-          family: font.family
-        })
-      })
-    }
-  })
-}
 
-registerFonts()
-
-export const fontFamilies = Object.keys(FONTS)
-  .reduce((acc, key) => ({ ...acc, [key]: FONTS[key].family }), {})
+export const fontFamilies = Object.keys(fonts)
+  .reduce((acc, key) => ({ ...acc, [key]: fonts[key].family }), {})
