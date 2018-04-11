ea23e69552271c76a9af212c926495faea757971
Diego Muracciole <diegomuracciole@gmail.com>
2018-04-11 10:47:46 -0300

    Use custom hyphenation callback (#27)

---
+++ src/lib/hyphenation.js
@@ -0,0 +1,33 @@
+import createHyphenator from 'hyphen'
+import patterns from 'hyphen/patterns/de-ch'
+
+const SOFT_HYPHEN = '\u00AD'
+
+const hypenator = createHyphenator(patterns)
+
+const hyphenateString = (string) => {
+  if (string.includes(SOFT_HYPHEN)) {
+    return string.split(SOFT_HYPHEN)
+  }
+
+  return hypenator(string).split(SOFT_HYPHEN)
+}
+
+const hyphenateWord = (glyphString) => {
+  const hyphenated = hyphenateString(glyphString.string)
+
+  let index = 0
+  const parts = hyphenated.map(part => {
+    const res = glyphString.slice(index, index + part.length)
+    index += part.length
+    return res
+  })
+
+  return parts
+}
+
+const hyphenationCallback = (words) => (
+  words.map(word => hyphenateWord(word))
+)
+
+export default hyphenationCallback
