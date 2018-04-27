0c1c3266a0245729bc5eae1143179abec7f38e93
Diego Muracciole <diegomuracciole@gmail.com>
2018-04-27 14:41:14 -0300

    Fix note background (#81)
    * Adjust BlockQuote to support multiple paragraphs
    
    * Rollback example

--- src/components/BlockQuote.js
+++ src/components/BlockQuote.js
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { Children } from 'react'
 import { Text, View, StyleSheet } from '@react-pdf/core'
 
 const styles = StyleSheet.create({
-  text: {
-    padding: 10,
+  container: {
     marginBottom: 3,
     backgroundColor: '#f7f7f7'
+  },
+  text: {
+    padding: 10
   }
 })
-
-const BlockQuote = ({ children }) => (
-  <View>{children}</View>
-)
 
 const BlockQuoteText = ({ children, ...props }) => (
   <Text style={styles.text}>{children}</Text>
 )
+
+const BlockQuote = ({ children }) => {
+  const texts = Children.toArray(children).filter(child => child.type === BlockQuoteText)
+  const legend = Children.toArray(children).filter(child => child.type !== BlockQuoteText)
+
+  return (
+    <View>
+      <View style={styles.container}>
+        {texts}
+      </View>
+      {legend}
+    </View>
+  )
+}
 
 BlockQuote.Text = BlockQuoteText
 
