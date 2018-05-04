f2255878fc26ece36b0fad43a3a9acf4f53edc8e
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-05-04 22:12:12 +0200

    concurrent render to file test

--- test/renderToFile.js
+++ test/renderToFile.js
@@ -2,6 +2,13 @@ const rw = require('rw')
 const fs = require('fs')
 const path = require('path')
 const fetch = require('node-fetch')
+const { queue } = require('d3-queue')
+
+const {
+  CONCURRENCY = 1,
+  BASE_URL = 'http://localhost:3007',
+  OFFSET = 0
+} = process.env
 
 // GraphQL Query
 // {
@@ -16,25 +23,31 @@ const fetch = require('node-fetch')
 // }
 const docs = JSON.parse(rw.readFileSync('/dev/stdin', 'utf8')).data.documents.nodes
   .filter(doc => !doc.meta.template || doc.meta.template === 'article')
-  // .slice(231)
+  .slice(OFFSET)
 
-async function fetchDocs (docs) {
-  for (const doc of docs) {
-    await fetch(`http://localhost:3007${doc.meta.path}`)
-      .then(res => res.buffer())
-      .then(buffer => {
-        const file = path.join(
-          __dirname,
-          'out',
-          `${doc.meta.path.replace(/\//g, ' ').trim().replace(/ /g, '-')}.pdf`
-        )
-        fs.writeFileSync(file, buffer)
-      })
-      .catch(err => {
-        console.error('Broken', doc.meta.path, err)
-        process.exit()
-      })
-  }
+function fetchDoc (doc) {
+  return fetch(`${BASE_URL}${doc.meta.path}`)
+    .then(res => res.buffer())
+    .then(buffer => {
+      const file = path.join(
+        __dirname,
+        'out',
+        `${doc.meta.path.replace(/\//g, ' ').trim().replace(/ /g, '-')}.pdf`
+      )
+      fs.writeFileSync(file, buffer)
+      console.log('Done', doc.meta.path)
+    })
+    .catch(err => {
+      console.error('Broken', doc.meta.path, err)
+      process.exit()
+    })
 }
 
-fetchDocs(docs)
+const q = queue(CONCURRENCY)
+docs.forEach(doc => q.defer((callback) => {
+  fetchDoc(doc).then(() => callback(), () => callback())
+}))
+q.awaitAll((error) => {
+  if (error) throw error
+  console.log('All Done')
+})
